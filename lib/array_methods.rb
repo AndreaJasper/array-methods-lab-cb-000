e16f4d44f7cca100483f756ec5b21a7363eb1791
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-08 15:57:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,29 +1,29 @@
 def using_include(array, element)
-  list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+  list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
   list.include?("wow")
 end
 
 def using_sort(array)
-  list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+  list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
   list.sort
 end
 
 def using_reverse(array)
-  list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+  list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
   list.reverse!
 end
 
 def using_first(array)
-  list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+  list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
   list.first
 end
 
 def using_last(array)
-  list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+  list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
   list.last
 end
 
 def using_size(array)
-    list = ["wow", "I", "am", "arrays!", "hello", "goodbye"]
+    list = ["wow", "I", "am", "arrays!", "hello", "arrays!"]
     list.size
 end
